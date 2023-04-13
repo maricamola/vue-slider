f87fd166c930134344372113ed0cd0cff492ba63
maricamola <maricamola98@libero.it>
2023-04-13 17:50:38 +0200

    thumb created

--- js/script.js
+++ js/script.js
@@ -13,6 +13,13 @@ createApp({
         'img/foto4.jpg',
       ],
       counter:0,
+      
+      image:[
+        'img/foto1.jpg',
+        'img/foto2.jpg',
+        'img/foto3.jpg',
+        'img/foto4.jpg',
+      ],
     }
   },
 
