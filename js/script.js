e45ada8bd36c98ac7ff2e6b4802b3e1b9e2a0de4
maricamola <maricamola98@libero.it>
2023-04-13 16:56:28 +0200

    setInterval created

--- js/script.js
+++ js/script.js
@@ -10,14 +10,25 @@ createApp({
         'img/foto1.jpg',
         'img/foto2.jpg',
         'img/foto3.jpg',
-        'img/foto4.jpg'
+        'img/foto4.jpg',
       ],
-      counter:0
+      counter:0,
     }
   },
 
   methods:{
-  }
+    arrow(next){
+      if(next) this.counter++;
+      else this.counter--
+      if(this.counter === this.images.length) this.counter = 0;
+      if(this.counter < 0) this.counter = this.images.length -1;
+      console.log(this.counter)
+    }
+  },
 
-
+  mounted(){
+    setInterval(() => {
+      this.arrow(true);
+    }, 3000);
+  },
 }).mount('#app')
